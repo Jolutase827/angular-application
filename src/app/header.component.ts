013a40d58d1dada09413ebeddc3a75720fa4b9c5
jsug <jolutase104@gmail.com>
2024-10-17 10:45:07 +0200

    first commit

---
+++ src/app/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-header",
+  //template: `<h1> Hello World</h1>`, Not recomend do the template like this.
+  standalone: true,
+  templateUrl:`./header.component.html`,
+
+})
+
+export class HeaderComponent{
+
+}
